e11263dfe991a44930f7cd3a92e7869d1bbf34a6
Idalio Pessoa <idaliopessoa@gmail.com>
2023-03-30 20:35:47 -0300

    ajuste

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -9,11 +10,18 @@ Future main() async {
 
   await Permission.camera.request();
   await Permission.microphone.request();
-
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor:
+        Color.fromRGBO(9, 142, 197, 1), // navigation bar color
+    statusBarColor: Color.fromRGBO(9, 142, 197, 1), // status bar color
+  ));
   if (Platform.isAndroid) {
     await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
   }
-
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((value) => runApp(const MyApp()));
   runApp(const MyApp());
 }
 
@@ -26,7 +34,10 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: InAppWebViewPage());
+    return const MaterialApp(
+      home: InAppWebViewPage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
@@ -38,33 +49,35 @@ class InAppWebViewPage extends StatefulWidget {
 }
 
 class InAppWebViewPageState extends State<InAppWebViewPage> {
+  // ignore: unused_field
   InAppWebViewController? _webViewController;
   final initalUrl = URLRequest(
       url: Uri.parse(
-          "https://app-dev.certfy.tech/onboarding/autoid/c873938a-7c34-43a8-9218-66f6b45e3066/steps"));
+          "https://app-dev.certfy.tech/onboarding/app/queue/94612025-d6f5-4951-91f0-118016d92944"));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("InAppWebView")),
-        body: Column(children: <Widget>[
-          Expanded(
-            child: InAppWebView(
-                initialUrlRequest: initalUrl,
-                initialOptions: InAppWebViewGroupOptions(
-                  crossPlatform: InAppWebViewOptions(
-                    mediaPlaybackRequiresUserGesture: false,
-                  ),
+        body: SafeArea(
+      child: Column(children: <Widget>[
+        Expanded(
+          child: InAppWebView(
+              initialUrlRequest: initalUrl,
+              initialOptions: InAppWebViewGroupOptions(
+                crossPlatform: InAppWebViewOptions(
+                  mediaPlaybackRequiresUserGesture: false,
                 ),
-                onWebViewCreated: (InAppWebViewController controller) {
-                  _webViewController = controller;
-                },
-                androidOnPermissionRequest: (InAppWebViewController controller,
-                    String origin, List<String> resources) async {
-                  return PermissionRequestResponse(
-                      resources: resources,
-                      action: PermissionRequestResponseAction.GRANT);
-                }),
-          ),
-        ]));
+              ),
+              onWebViewCreated: (InAppWebViewController controller) {
+                _webViewController = controller;
+              },
+              androidOnPermissionRequest: (InAppWebViewController controller,
+                  String origin, List<String> resources) async {
+                return PermissionRequestResponse(
+                    resources: resources,
+                    action: PermissionRequestResponseAction.GRANT);
+              }),
+        ),
+      ]),
+    ));
   }
 }
